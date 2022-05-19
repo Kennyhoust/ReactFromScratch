f1af1dddd11462621c94842e21f03bed3277b21a
LAPTOP-UFU4D763\kenny <cs_hkwab@alumni.ust.hk>
2022-05-19 21:52:47 +0800

    add login page template

--- src/App.js
+++ src/App.js
@@ -6,20 +6,31 @@ class App extends React.Component {
 
 
         return (
-            <div>
-                <div  className = 'header1'>
 
-                    <div className ='start'>
-                    <a href="/">首頁</a>
-                    </div>
+                <div className="text-center">
+                    <main className="form-signin w-100 m-auto">
+                    <form>
+                        <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
 
-                    <div className ='end'>
-                        <a href='/'>關於我們</a>  
-                        <a href='/'>聯絡方式</a>   
-                        <a href='/'>登入 </a>
-                    </div>
+                        <div className="form-floating">
+                        <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
+                        <label for="floatingInput">Email address</label>
+                        </div>
+                        <div className="form-floating">
+                        <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
+                        <label>Password</label>
+                        </div>
+
+                        <div className="checkbox mb-3">
+                        <label>
+                            <input type="checkbox" value="remember-me"/> Remember me
+                        </label>
+                        </div>
+                        <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+                        <p className="mt-5 mb-3 text-muted">&copy; 2017–2022</p>
+                    </form>
+                    </main>
                 </div>
-            </div>
 
         )
     }
